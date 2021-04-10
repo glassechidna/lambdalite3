545f18699854942280f528931c41457baeeb6d8e
Aidan Steele <aidan.steele@glassechidna.com.au>
2021-04-10 10:34:41 +1000

    zig hax

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 set -euxo pipefail
 
-docker build -t lambdalite3 .
-cid=$(docker create lambdalite3)
-docker cp ${cid}:/app/lambdalite3 .
-docker rm ${cid}
+export CGO_ENABLED=1
+export GOOS=linux
+export CC=$(pwd)/zcc
 
-stackit up --region ap-southeast-2 --profile ge --template infra.yml --stack-name lambdalite3-demo
+go build -ldflags="-s -w"
+
+#docker build -t lambdalite3 .
+#cid=$(docker create lambdalite3)
+#docker cp ${cid}:/app/lambdalite3 .
+#docker rm ${cid}
+
+#stackit up --region ap-southeast-2 --profile ge --template infra.yml --stack-name lambdalite3-demo
